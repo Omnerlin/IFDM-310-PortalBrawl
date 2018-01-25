e9f1086fe3f943684cc01ca5c27c7d7e82ed9a9f
Alex Booher <alex.booher@yahoo.com>
2018-01-24 18:33:02 -0700

    Inital project commit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float maxMoveSpeed;
+    public float moveForce;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        // Update the player's movespeed based on input axis (-1 to 1)
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        rb2d.AddForce(new Vector2(moveForce * moveX, moveForce * moveY));
+
+        if(Mathf.Abs(rb2d.velocity.x) > maxMoveSpeed)
+        {
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxMoveSpeed, rb2d.velocity.y);
+        }
+        if (Mathf.Abs(rb2d.velocity.y) > maxMoveSpeed)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.Sign(rb2d.velocity.y) * maxMoveSpeed);
+        }
+
+        Debug.Log("Velocity X: " + rb2d.velocity.x + "Velocity Y: " + rb2d.velocity.y);
+    }
+}
