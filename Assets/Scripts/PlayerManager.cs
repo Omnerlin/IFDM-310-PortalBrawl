9959830f9732fd994a2f429eaf7395513afd7c86
Alex Booher <alex.booher@yahoo.com>
2018-02-04 23:56:42 -0700

    Added some basic camera movement and reorganized a bit

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Rewired;
+using Cinemachine;
 
 public class PlayerManager : MonoBehaviour
 {
 
     public static PlayerManager instance;
+    public GameObject cameraFollowGroup;
 
     private List<int> assignedControllers = new List<int>();
     public GameObject playerPrefab;
@@ -27,28 +29,35 @@ public class PlayerManager : MonoBehaviour
     private void Update()
     {
         // We need to test if any players want to join the game
-        IList <Rewired.Player> players = Rewired.ReInput.players.GetPlayers(false);
+        IList <Rewired.Player> players = ReInput.players.GetPlayers(false);
         for(int i = 0; i < players.Count; i++)
         {
             // Assign the player 
             if(!players[i].isPlaying && players[i].GetButtonDown("Start"))
             {
-                GameObject pFab = Instantiate(playerPrefab);
-                PlayerControl pControl = pFab.GetComponent<PlayerControl>();
-                pControl.player = Rewired.ReInput.players.GetPlayer(i);
-                pControl.player.isPlaying = true;
+                AddPlayer(i);                
             }
         }
     }
 
     // Add a controller to the list of initialized controls
-    private void AddControllerAndPlayer(int ControlID)
+    private void AddPlayer(int playerID)
     {
-        assignedControllers.Add(ControlID);
-        GameObject player = Instantiate(playerPrefab);
-        playerPrefab.GetComponent<Player>().playerNumber = assignedControllers.Count;
-        player.GetComponent<PlayerControl>().controllerNumber = ControlID;
-        Debug.Log("Creating Player " + assignedControllers.Count + " using controller ID " + ControlID);
+        GameObject pFab = Instantiate(playerPrefab);
+        PlayerControl pControl = pFab.GetComponent<PlayerControl>();
+        pControl.player = ReInput.players.GetPlayer(playerID);
+        pControl.player.isPlaying = true;
+
+        // Add the player to the group of objects to be tracked by the camera
+        // while keeping the other targets
+        CinemachineTargetGroup groupComp = cameraFollowGroup.GetComponent<CinemachineTargetGroup>();
+        List<CinemachineTargetGroup.Target> group = new List<CinemachineTargetGroup.Target>(groupComp.m_Targets);
+        CinemachineTargetGroup.Target newTarget = new CinemachineTargetGroup.Target();
+        newTarget.target = pFab.transform;
+        newTarget.weight = 1.0f;
+        group.Add(newTarget);
+
+        groupComp.m_Targets = group.ToArray();
     }
 
 }
