18f63ea66d4a98bd244e3e9181ea99765b8d7fc4
crespinv <crespinv@unm.edu>
2018-02-04 18:37:06 -0700

    Commit to add my files before pull

---
+++ Assets/Scripts/PlayerConTemp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConTemp : MonoBehaviour {
+    public GunController theGun;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            theGun.isFiring = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            theGun.isFiring = false;
+        }
+	}
+}
