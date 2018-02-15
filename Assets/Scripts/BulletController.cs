76a59f5751d21402b81e2c17efa69ac11e4c2026
Alex Booher <alex.booher@yahoo.com>
2018-02-14 22:41:10 -0700

    Merged more of Vincent's stuff with mine to prepare for the main line

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -2,16 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletController : MonoBehaviour {
+public class BulletController : MonoBehaviour
+{
     public float speed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector2.left * speed* Time.deltaTime);
-	}
+    public float lifeTime;
+    public int damageForEnemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            other.gameObject.GetComponent<EnemyHp>().HurtEnemy(damageForEnemy);
+        }
+        else if (other.gameObject.tag.Equals("Player"))
+        {
+            // Ignore contact with the player since they shot it (Will need to change this later)
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), other.collider);
+            return;
+        }
+        Destroy(gameObject);
+    }
 }
