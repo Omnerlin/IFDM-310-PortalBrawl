eb2d5fd0e39321e3b6fed111eddbaebb8250babd
Alex Booher <alex.booher@yahoo.com>
2018-02-14 21:50:20 -0700

    Merged in some of Vincent's stuff

---
+++ Assets/Scripts/EnemyHp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHp : MonoBehaviour {
+
+    public int hP;
+    public int currentHP;
+
+	// Use this for initialization
+	void Start () {
+        currentHP = hP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(currentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+		
+	}
+    public void HurtEnemy(int damage)
+    {
+        currentHP -= damage;
+    }
+}
