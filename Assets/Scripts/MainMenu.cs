5850b01b3542f9f1550f2e6f033694301d52513b
Vincent Crespin <crespinv@unm.edu>
2018-02-21 10:24:21 -0700

    Main Menu Up Date

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using Rewired;
 
 public class MainMenu : MonoBehaviour {
-    public bool isStart;
+    public int index = 0;
+    public int totalOptions;
+    public float yOffset;
+
+    public Rewired.Player player { get; set;}
+
+    //public bool isStart;
+
+    public void Awake()
+    {
+        player = Rewired.ReInput.players.GetPlayer(0);
+    }
 
     private void OnMouseUp()
 
     {
 
-        if (isStart)
+        /*if (isStart)
 
         {
 
@@ -24,17 +36,61 @@ public class MainMenu : MonoBehaviour {
 
             Application.Quit();
 
-        }
+        }*/
 
     }
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start ()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (player.GetAxis("Move Vertical") < 0)
+        {
+            if (index <= (totalOptions - 1))
+            {
+                index++;
+                Vector2 postion = transform.position;
+                postion.y -= yOffset;
+                transform.position = postion;
+            }
+        }
+        if (player.GetAxis("Move Vertical") > 0)
+        {
+            if ((index > 0))
+            {
+                index--;
+                Vector2 postion = transform.position;
+                postion.y += yOffset;
+                transform.position = postion;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if (player.GetButton("Start"))
+        {
+            if(index == 0)
+            {
+                SceneManager.LoadScene("CharacterSelection");
+            }
+            if(index == 1)
+            {
+                Debug.Log("Load game was selected, but is not available yet");
+            }
+            if (index == 2)
+            {
+                Debug.Log("Options were selected, but is not available yet");
+            }
+            if (index == 3)
+            {
+                Application.Quit();
+            }
+        }
+    }
 }
