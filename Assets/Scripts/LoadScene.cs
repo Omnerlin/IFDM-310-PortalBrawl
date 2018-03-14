3b42f301d54be7c72b3f387bc49aa16fc70aa71e
Alex <alex.booher@yahoo.com>
2018-03-14 14:13:42 -0600

    Loading a new scene with the load scene script instantiates a new sceneTransitionmanager if one doesn't exist

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -5,7 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour {
 
-	/**Scene must be added to the Build to be able to be loaded!*/
+    /**Scene must be added to the Build to be able to be loaded!*/
+
+    // The prefab used to instantiate a sceneTransitionCanvas if one doesn't exist
+    public GameObject sceneTransitionCanvasPrefab;
+
 	public string sceneToLoad;
 	public bool hasBeenPressed = false;
 
@@ -14,6 +18,14 @@ public class LoadScene : MonoBehaviour {
 		if (!hasBeenPressed) 
 		{
             //TODO: Change to actual scene
+
+            // If the sceneTransitionManager does not exist in this scene, go ahead and instantiate the transition canvas.
+            if(!SceneTransitionManager.Instance)
+            {
+                Instantiate(sceneTransitionCanvasPrefab);
+            }
+
+
             SceneTransitionManager.Instance.TransitionToScene(sceneToLoad, SceneTransitionManager.AnimationType.forward);
 			hasBeenPressed = true;
 		}
