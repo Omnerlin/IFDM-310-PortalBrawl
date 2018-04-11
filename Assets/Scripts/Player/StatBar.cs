bb233ebbe87b8e22ff4e2d9ee741847da5580de2
Anna Carey <careya42>
2018-04-11 14:42:25 -0600

    Health bars change color

--- Assets/Scripts/Player/StatBar.cs
+++ Assets/Scripts/Player/StatBar.cs
@@ -1,10 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatBar : MonoBehaviour {
 
 	public GameObject fullBar;
+
+	//private HealthGradient gradient;
+
+	Gradient g;
+	GradientColorKey[] gck;
+	GradientAlphaKey[] gak;
+
+
+	void Start()
+	{
+		//gradient = new HealthGradient ();
+		g = new Gradient ();
+		gck = new GradientColorKey[3];
+		gck [0].color = Color.red;
+		gck [0].time = 0.0F;
+		gck [1].color = Color.yellow;
+		gck [1].time = 0.5F;
+		gck [2].color = Color.green;
+		gck [2].time = 1.0F;
+
+		gak = new GradientAlphaKey[1];
+		gak [0].alpha = 1.0f;
+		gak [0].time = 0.0F;
+
+		g.SetKeys (gck,gak);
+	}
 
 	//Sets the bar to the correct percentage of currentHealth to maxHealth.
 	//could also display currentHealth in a text component on the bar.
@@ -21,6 +48,10 @@ public class StatBar : MonoBehaviour {
             return;
         }
 
-		fullBar.transform.localScale = new Vector3 ((currentStat / maxStat), 1, 1);
+		float barPercentFull = currentStat / maxStat;
+		fullBar.transform.localScale = new Vector3 (barPercentFull, 1, 1);
+		fullBar.GetComponent<Image>().color = g.Evaluate(barPercentFull);
+
 	}
+		
 }
