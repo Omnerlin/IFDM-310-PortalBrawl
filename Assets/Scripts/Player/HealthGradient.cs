c5c8f6ef55553f6c48a475842e5a78a1a0097445
Anna Carey <careya42>
2018-04-12 04:35:22 -0600

    Menu screen looks good now.

--- Assets/Scripts/Player/HealthGradient.cs
+++ Assets/Scripts/Player/HealthGradient.cs
@@ -6,22 +6,6 @@ public class HealthGradient {
 
 	public HealthGradient()
 	{
-		Gradient g;
-		GradientColorKey[] gck;
-		GradientAlphaKey[] gak;
-		g = new Gradient ();
-		gck = new GradientColorKey[3];
-		gck [0].color = Color.red;
-		gck [0].time = 0.0F;
-		gck [1].color = Color.yellow;
-		gck [1].time = 0.5F;
-		gck [2].color = Color.green;
-		gck [2].time = 1.0F;
-
-		gak = new GradientAlphaKey[1];
-		gak [0].alpha = 1.0f;
-		gak [0].time = 0.0F;
-
-		g.SetKeys (gck,gak);
+		
 	}
 }
