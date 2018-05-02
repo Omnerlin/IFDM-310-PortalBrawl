ccc3f96a7be04d0e8bd4ea7fdfa8d5396fc853e6
Alex Booher <alex.booher@yahoo.com>
2018-05-01 23:59:42 -0600

    adding more code

--- Assets/CameraBoundaries.cs
+++ Assets/CameraBoundaries.cs
@@ -16,6 +16,13 @@ public class CameraBoundaries : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        cam = GetComponent<Camera>();
+
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float camHalfHeight = cam.orthographicSize;
+        float camHalfWidth = screenAspect * camHalfHeight;
+        float camWidth = 2.0f * camHalfWidth;
+        float camHeight = camHalfHeight * 2;
 
         colliders = new BoxCollider2D[4]; // Top, Right, Bottom, Left
 
@@ -28,16 +35,19 @@ public class CameraBoundaries : MonoBehaviour {
             {
                 case 0:
                     col.offset = transform.InverseTransformDirection(cam.ViewportToWorldPoint(new Vector3(0.5f, 1)));
-                    col.size = new Vector2(1, 1);
+                    col.size = new Vector2(camWidth, boxThiccness);
                     break;
                 case 1:
                     col.offset = cam.ViewportToWorldPoint(new Vector3(1, 0.5f));
+                    col.size = new Vector2(boxThiccness, camHeight);
                     break;
                 case 2:
                     col.offset = cam.ViewportToWorldPoint(new Vector3(0.5f, 0));
+                    col.size = new Vector2(camWidth, boxThiccness);
                     break;
                 case 3:
                     col.offset = cam.ViewportToWorldPoint(new Vector3(0, 0.5f));
+                    col.size = new Vector2(boxThiccness, camHeight);
                     break;
             }
         }
