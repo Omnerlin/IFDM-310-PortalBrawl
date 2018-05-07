ffb311efcf168936fa57757ba82b37f9339c5ad6
Vincent Crespin <crespinv@unm.edu>
2018-05-07 10:05:07 -0600

    made control screen page

---
+++ Assets/Scripts/Menuing/ControlScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using Rewired;
+using UnityEngine.UI;
+
+public class ControlScreen : MonoBehaviour {
+
+    public IList<Rewired.Player> players { get; set; }
+    public AudioSource sounds;
+
+    private void Awake()
+    {
+        players = ReInput.players.GetPlayers(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Rewired.Player player in players)
+        {
+            if (player.GetButtonDown("Start") || player.GetButtonDown("XButton"))
+            {
+                Debug.Log("loading level");
+                SceneManager.LoadScene("Alexlevel");
+                sounds.Play();
+            }
+        }
+
+
+    }
+}
